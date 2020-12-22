301dbb24f993c6409184b79a776238344ea82f5c
strHeelJack <jackfrasiercomedy@gmail.com>
2020-12-22 11:08:55 -0500

    this is my comments in a .py file on the solver -Jack

---
+++ projects/sudokuSolver.py
@@ -0,0 +1,26 @@
+#! python3
+
+====================================
+        Sudoku Solver
+====================================
+
+
+#   Article that may help us have a good project skeleton.
+#       http://pi.math.cornell.edu/~mec/Summer2009/Mahmood/Count.html
+
+
+    #       Make a Sudoku Game Generator
+
+            #   Using range(9) generate a 9x9 grid
+            #   Where all numbers are ==
+
+    #       Make a Sudoku Game Solver
+
+            #   Find the boxes, columns and rows with the most solved numbers
+            #   Make lists for all soved numbers in given rows, columns and boxes
+            #   Make lists for all numbers != rows, boxes and columns
+            #   When all numbers are solved, put them in a list of solved numbers ont to be considered. (9 in all)
+            #   Make a list for all soved Boxes, rows, columns and numbers
+            #   Game is finished when all rows, columns, boxes and numbers and blanks are solved correctly.
+
+            #   Make errors for wrong blanks.
